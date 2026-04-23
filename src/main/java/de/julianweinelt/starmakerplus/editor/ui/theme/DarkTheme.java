b5a4e47324ca58443e0bf64913f7be29a581475b
Julian Weinelt <julian.weinelt@outlook.de>
2026-04-23 21:08:27 +0200

    ui: add basic ui colors for dark theme

---
+++ src/main/java/de/julianweinelt/starmakerplus/editor/ui/theme/DarkTheme.java
@@ -0,0 +1,13 @@
+package de.julianweinelt.starmakerplus.editor.ui.theme;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.util.Enumeration;
+
+public class DarkTheme {
+    public static final Color ACCENT = new Color(0x7C, 0xB9, 0xF0);
+    public static final Color BORDER = new Color(0x45, 0x45, 0x60);
+}
