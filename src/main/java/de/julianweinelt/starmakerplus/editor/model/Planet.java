7aa65fe63a7c432b5ec13ec64e47f8aa5c9132cf
Julian Weinelt <julian.weinelt@outlook.de>
2026-04-23 21:11:14 +0200

    api: add json model for planet data serialization

---
+++ src/main/java/de/julianweinelt/starmakerplus/editor/model/Planet.java
@@ -0,0 +1,74 @@
+package de.julianweinelt.starmakerplus.editor.model;
+
+import com.google.gson.annotations.SerializedName;
+import de.julianweinelt.starmakerplus.editor.util.WorldCloudConfig;
+import de.julianweinelt.starmakerplus.serialize.GSONCreator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Planet {
+    @SerializedName("parent_system")
+    private String parentSystem = "";
+    @SerializedName("orbit_data")
+    private OrbitData orbitData = new OrbitData();
+
+    private float gravity = 0.058F;
+    @SerializedName("atmosphere_pressure")
+    private int atmospherePressure = 1;
+
+    private Temperature temperature = new Temperature();
+
+    @SerializedName("wind")
+    private float windStrength = 0.0F;
+
+    @SerializedName("day_lenght")
+    private int dayLength = 24000;
+
+    private boolean breathable = false;
+    @SerializedName("solar_radiation")
+    private boolean solarRadiation = false;
+    @SerializedName("corrosive_atmo")
+    private boolean corrosiveAtmosphere = false;
+
+    @SerializedName("sun_brightness")
+    private float sunBrightness = 0.5F;
+    @SerializedName("star_brightness")
+    private float starBrightness = 0.5F;
+
+    @SerializedName("sky")
+    private Color skyColor = new Color(127, 255, 212);
+    @SerializedName("fog")
+    private Color fogColor = new Color(127, 255, 212);
+    @SerializedName("cloud")
+    private WorldCloudConfig cloudColor = new WorldCloudConfig();
+    @SerializedName("light")
+    private Color lightColor = new Color(255, 50, 50);
+
+    @SerializedName("world_data")
+    private WorldData worldData = new WorldData();
+
+    private List<String> biomes = new ArrayList<>();
+
+    @SerializedName("sun_size")
+    private float sunSize = 5.0F;
+    private boolean precipitation = false;
+    private boolean unreachable = false;
+    @SerializedName("tidallyLocked")
+    private boolean stopDayNightCycle = false;
+
+    @Override
+    public String toString() {
+        return GSONCreator.create().toJson(this);
+    }
+}
