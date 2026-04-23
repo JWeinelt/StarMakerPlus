91239c0f4de59a2ffaf4d9fc471e0451bd4bb804
Julian Weinelt <julian.weinelt@outlook.de>
2026-04-23 22:25:09 +0200

    license: remove double-space in copyright text

--- src/main/java/de/julianweinelt/starmakerplus/editor/model/StarType.java
+++ src/main/java/de/julianweinelt/starmakerplus/editor/model/StarType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026  Julian Weinelt
+ * Copyright (C) 2026 Julian Weinelt
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
